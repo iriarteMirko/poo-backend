05bfd80db30e12f68ae20a8563ed31c9fb1aea6a
Mirko Iriarte <20184037@aloe.ulima.edu.pe>
2024-05-20 01:49:52 -0500

    cambio8

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from usuario import Usuario
 from profesor import Profesor
 from estudiante import Estudiante
 from curso import CursoFactory
@@ -9,21 +8,20 @@ estudiante2 = Estudiante('Maria', 'Gomez', 'c@c', '789', '2', 'Ing. de Sistemas'
 estudiante1.get_datos()
 estudiante2.get_datos()
 
-
 profesor1 = Profesor('Pedro', 'Garcia', 'b@b', '456', 'Ing. de Sistemas', 'UNI')
 profesor1.get_datos()
 
-
 fabrica = CursoFactory()
 curso1 = fabrica.crear_curso('Curso 1', 'Descripcion 1', profesor1)
-curso1.get_datos()
-
 curso2 = fabrica.crear_curso('Curso 2', 'Descripcion 2', profesor1)
+
+curso1.get_datos()
 curso2.get_datos()
 
 estudiante1.matricular_curso(curso1)
 estudiante2.matricular_curso(curso1)
 estudiante1.matricular_curso(curso2)
+
 print(estudiante1.get_cursos())
 print(estudiante2.get_cursos())
 print('\n')
